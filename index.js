fdd7a75cb3df35e5985b59e7abadf23464597468
Danny Lee <dreamer@panix.com>
2019-11-05 21:01:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+// create a function with has argument 'el' and evaluates whether 'el'
+// equals "W", if so return a truthy or falsey result.
+const isAWin = el => el.result === "W";
+
+// takes an array of javascript objects and passes them to the .find()
+// and returns "objArr.year" if any are returned by the .find() or it
+// returns "undefined" to the caller.
+function superbowlWin(objArr) {
+  let win = objArr.find(isAWin);
+  return (win ? win.year : undefined);
 }
+
